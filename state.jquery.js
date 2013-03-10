eb96a74ed0537f87795ab1cb8578274b2c8945a5
Disparity <disparity-github@yandex.ru>
2013-03-10 12:38:42 +0700

    Re-organize code

--- state.jquery.js
+++ state.jquery.js
@@ -2,63 +2,79 @@
  * jQuery state plugin
  *
  * @author Disparity <disparity-github@yandex.ru>
- * @version 0.1.3
+ * @version 0.1.4
  */
 (function($, undefined) {
-	var stateParser = function(stateElem, setOrUnset) {
-		var states = {};
-		if (typeof stateElem === "string") {
-			$.each($.grep(stateElem.split(" "), function(stateName) {
-				return $.trim(stateName) !== "";
-			}), function(index, stateName) {
-				states[stateName] = setOrUnset;
-			});
-		}
-		return states;
-	}
+	var state = {
+		change:
+			function($element, stateName, setOrUnset) {
+				$element.toggleClass(stateName, setOrUnset);
+			},
 
-	var stateNotificator = function($element, stateName, setOrUnset) {
-		if (setOrUnset === undefined) {
-			setOrUnset = $element.is("." + stateName);
-		}
-		// @todo how many events to fire? ...
-		var notificationTypes = [
-			"change.state",
-			(setOrUnset ? "set" : "remove") + ".state",
-			"change.state-" + stateName,
-			(setOrUnset ? "set" : "remove") + ".state-" + stateName
-		];
+		is:
+			function($element, stateName) {
+				return $element.is("." + stateName);
+			},
 
-		$.each(notificationTypes, function(index, notificationType) {
-			var event = $.Event(notificationType, {state: {name: stateName, value: setOrUnset}});
-			event.preventDefault();
-			$element.trigger(event);
-		});
-	}
+		parser:
+			function(stateElem, setOrUnset) {
+				var states = {};
+				if (typeof stateElem === "string") {
+					$.each($.grep(stateElem.split(" "), function(stateName) {
+						return $.trim(stateName) !== "";
+					}), function(index, stateName) {
+						states[stateName] = setOrUnset;
+					});
+				}
+				return states;
+			},
 
-	var stateProcessor = function($element, stateName, setOrUnset) {
-		if (typeof setOrUnset !== "boolean" && setOrUnset !== undefined) {
-			throw {message: "Invalid argumment type"}; // @todo fix message
-		}
-		if ($element.isInState(stateName) === setOrUnset) {
-			return;
-		}
-		$element.toggleClass(stateName, setOrUnset);
-		stateNotificator($element, stateName, setOrUnset);
-	}
+		notificator:
+			function($element, stateName, setOrUnset) {
+				if (setOrUnset === undefined) {
+					setOrUnset = $element.isInState(stateName);
+				}
+				// @todo how many events to fire? ...
+				var notificationTypes = [
+					"change.state",
+					(setOrUnset ? "set" : "remove") + ".state",
+					"change.state-" + stateName,
+					(setOrUnset ? "set" : "remove") + ".state-" + stateName
+				];
 
-	$.fn.toggleState = function(state, setOrUnset) {
-		var states = stateParser(state, setOrUnset);
+				$.each(notificationTypes, function(index, notificationType) {
+					var event = $.Event(notificationType, {state: {name: stateName, value: setOrUnset}});
+					event.preventDefault();
+					$element.trigger(event);
+				});
+			},
+
+		processor:
+			function($element, stateName, setOrUnset) {
+				if (typeof setOrUnset !== "boolean" && setOrUnset !== undefined) {
+					throw {message: "Invalid argumment type"}; // @todo fix message
+				}
+				if ($element.isInState(stateName) === setOrUnset) {
+					return;
+				}
+				this.change($element, stateName, setOrUnset);
+				this.notificator($element, stateName, setOrUnset);
+			}
+	};
+
+
+	$.fn.toggleState = function(states, setOrUnset) {
+		states = state.parser(states, setOrUnset);
 		return this.each(function(index, element) {
-			$.each(states, function(stateName, state) {
-				stateProcessor($(element), stateName, state);
+			$.each(states, function(stateName, setOrUnset) {
+				state.processor($(element), stateName, setOrUnset);
 			});
 		});
 	}
 
-	$.fn.addState = function(state) {return this.toggleState(state, true);}
-	$.fn.removeState = function(state) {return this.toggleState(state, false);}
-	$.fn.isInState = function(stateName) {return this.is("." + stateName);}
+	$.fn.addState = function(states) {return this.toggleState(states, true);}
+	$.fn.removeState = function(states) {return this.toggleState(states, false);}
+	$.fn.isInState = function(stateName) {return state.is(this, stateName);}
 
 	$.fn.loadStates = function(dataKey) {
 		return this.each(function() {
@@ -67,8 +83,8 @@
 		});
 	}
 
-	$.fn.joinByState = function(state) {
-		var states = stateParser(state);
+	$.fn.joinByState = function(states) {
+		states = state.parser(states);
 		var $elements = this;
 		$.each(states, function(stateName) {
 			$elements.bind("set.state-" + stateName, function() {
@@ -76,5 +92,12 @@
 			});
 		});
 		return this;
+	}
+
+	$.fn.state = function(states, setOrUnset) {
+		if (setOrUnset === undefined && !$.isPlainObject(states)) {
+			return this.isInState(states);
+		}
+		return this.toggleState(states, setOrUnset);
 	}
 })(jQuery);
